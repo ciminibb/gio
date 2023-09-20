acb85ad37770099062d9b0ab8cfc886617b04cc3
Ben Cimini <ciminibb@mail.uc.edu>
2023-09-19 22:01:25 -0400

    programming server, around threads checkpoint

--- project-1/web-server/WebServer.java
+++ project-1/web-server/WebServer.java
@@ -17,7 +17,23 @@ import java.util.*;
 // "final." It cannot be extended or inherited.
 public final class WebServer {
     public static void main(String argv[]) throws Exception {
+        // Set port number to an arbitrary value between 1024 and 49151. Port
+        // 6789 is used for this project. Crucially, this port number must be
+        // used for requests to my server.
+        int port = 6789;
 
+        // Create welcomeSocket, an object that listens for a client on the port
+        // specified above: 6789.
+        ServerSocket welcomeSocket = new ServerSocket(port);
+
+        // Process HTTP requests in an infinite loop, per the following.
+        while (true) {
+            // Create connectionSocket when the port receives a request from
+            // some client. There is now a pipe ready for transport.
+            Socket connectionSocket = welcomeSocket.accept();
+
+            // NEW THREADS AND STREAMS? SYNTHESIZE BOTH INSTRUCTIONS!
+        }
     }
 }
 
