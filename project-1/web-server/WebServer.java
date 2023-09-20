ea8ebbe1434ac9a298f66e3c161f6ebdf3e54339
Ben Cimini <ciminibb@mail.uc.edu>
2023-09-19 21:22:57 -0400

    given code copied and commented

--- project-1/web-server/WebServer.java
+++ project-1/web-server/WebServer.java
@@ -2,3 +2,28 @@
  * Project 1
  * Ben Cimini
  */
+
+
+
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+
+
+// WebServer is a public class whose inheritance is restricted by keyword
+// "final." It cannot be extended or inherited.
+public final class WebServer {
+    public static void main(String argv[]) throws Exception {
+
+    }
+}
+
+
+// HttpRequest (inheritance restricted) is passed to the constructor of any new
+// thread created.
+final class HttpRequest implements Runnable {
+    
+}
