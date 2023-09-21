2e0d5656a46f1ee447733e59f46c574e2f768e74
Ben Cimini <ciminibb@mail.uc.edu>
2023-09-21 16:07:48 -0400

    first compilation checkpoint

--- project-1/web-server/WebServer.java
+++ project-1/web-server/WebServer.java
@@ -16,6 +16,7 @@ import java.util.*;
 // WebServer is a public class whose inheritance is restricted by keyword
 // "final." It cannot be extended or inherited.
 public final class WebServer {
+    // All main() does is process messages!
     public static void main(String argv[]) throws Exception {
         // Set port number to an arbitrary value between 1024 and 49151. Port
         // 6789 is used for this project. Crucially, this port number must be
@@ -50,5 +51,66 @@ public final class WebServer {
 // HttpRequest (inheritance restricted) is passed to the constructor of any new
 // thread created.
 final class HttpRequest implements Runnable {
-    
+    // The HTTP specification requires each line in requests/responses to
+    // conclude with a carriage return (cr = \r) and a line feed (lf = \n). So,
+    // we'll store those characters in a variable which is global to the class
+    // and impossible to change. The variable socket stores a reference to the
+    // connection socket, which was passed into the constructor of this class.
+    final static String crlf = "\r\n";
+    Socket socket;
+
+    // The HttpRequest constructor simply sets the objects socket as the one
+    // passed in.
+    public HttpRequest(Socket socket) throws Exception {
+        this.socket = socket;
+    }
+
+    // We define a run() method with explicit error catching, in compliance with
+    // the Runnable interface. To this point, we've simply been throwing
+    // exceptions, as noted in method declarations.
+    public void run() {
+        // Processing occurs in processRequest method, this one is merely a
+        // wrapper meant to catch its exceptions.
+        try {
+            processRequest();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void processRequest() throws Exception {
+        // Initialize stream variables to reference the socket's input and
+        // output streams. These can be retrieved from the object's socket,
+        // itself. The output stream will require a type cast.
+        InputStream iStream = socket.getInputStream();
+        DataOutputStream oStream = (DataOutputStream) socket.getOutputStream();
+
+        // The buffered reader can't read the input stream directly. It needs a
+        // filter to convert the bytes to characters.
+        BufferedReader bReader =
+            new BufferedReader(new InputStreamReader(iStream));
+
+        // Get the request line of the HTTP request using the readline() method
+        // of the buffered reader. It will only grab one line at a time, because
+        // extraction halts at the carriage-return-line-feed sequence.
+        String requestLine = bReader.readLine();
+        
+        System.out.println();            // Output a blank line.
+        System.out.println(requestLine); // Output the request line.
+
+        // Display header lines in a loop which terminates when an empty line is
+        // read. It is okay that the headerLine variable is overwritten in each
+        // iteration, we don't need those lines other than to display.
+        String headerLine = null;
+        while ((headerLine = bReader.readLine()).length() != 0) {
+            System.out.println(headerLine);
+        }
+
+        // When processing has concluded, close the open streams and socket to
+        // avoid resource leaks.
+        oStream.close();
+        bReader.close();
+        socket.close();
+    }
 }
